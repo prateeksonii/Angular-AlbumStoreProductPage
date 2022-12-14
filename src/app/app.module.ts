7672be3a3cac6b4d3aaaea969a3151d88ac6cfc7
Prateek Soni <psoni@milestechnologies.com>
2022-12-14 17:23:25 -0500

    complete part 7
    Signed-off-by: Prateek Soni <psoni@milestechnologies.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,15 @@ import { AppComponent } from "./app.component";
 import { ProductPageComponent } from "./product-page/product-page.component";
 import { ProductDescriptionComponent } from "./product-description/product-description.component";
 import { ProductService } from "./product.service";
-import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { ProductTracklistingComponent } from "./product-tracklisting/product-tracklisting.component";
+import { ProductListComponent } from "./product-list/product-list.component";
+import { RouterModule, Routes } from "@angular/router";
+
+const appRoutes: Routes = [
+  { path: "products", component: ProductListComponent },
+  { path: "product/:id", component: ProductPageComponent },
+  { path: "", redirectTo: "products", pathMatch: "full" },
+];
 
 @NgModule({
   declarations: [
@@ -22,6 +29,7 @@ import { ProductListComponent } from './product-list/product-list.component';
     BrowserModule,
     FormsModule,
     HttpModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [ProductService],
   bootstrap: [AppComponent],
